bad955c87176b9fa42701136ccac8cc57085ad0a
vimata <nhlapo.d@live.com>
2018-10-21 13:21:54 +0100

    add comment and likes routes

--- validation/login.js
+++ validation/login.js
@@ -12,7 +12,7 @@ module.exports = function validateLoginInput(data) {
   }
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must at least 6 characters';
+    errors.password = 'Password must be at least 6 characters';
   }
 
   if (Validator.isEmpty(data.email)) {
